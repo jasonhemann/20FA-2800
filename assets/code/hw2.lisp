1b1550f4e6d3a35e19b91300240df9edea323e82
Jason Hemann <jasonhemann@gmail.com>
2020-09-17 20:12:06 -0400

    pushing up correctinos

--- assets/code/hw2.lisp
+++ assets/code/hw2.lisp
@@ -46,8 +46,8 @@
 ;;; and a true list and returns a new list with the second symbol
 ;;; inserted after each occurrence of the first symbol.
 
-(check= (insert-right 'x 'y '(x z z x y x)) '(x y z z x y y x y))
 
+(check= (insert-right 'x 'y '(x z z x y x)) '(x y z z x y y x y))
 
 #|
 
@@ -62,11 +62,16 @@ pairs of associated values. For example, the following is an
 association list:
 
 ((A . 5) (B . (1 2)) (C . A))
+
+BTW, you may find the built-in ALIST and ALISTP of use to you. 
  
 |# 
 
 ;;; 3. Write MY-ASSOC, your own implementation of the lisp ASSOC
 ;;; function. You should not use ASSOC anywhere in your definition. 
+
+(defdata maybe-pair (oneof nil cons))
+
 
 
 #| 
@@ -95,11 +100,15 @@ use REMOVE.
 ;;; and recursively exchanges each CAR with its CDR.
 
 
+
+(check= (mirror 'a) 'a)
 (check= (mirror '((g h (a . b) . (c . d)) . (e . f)))
-	((f . e) (((d . c) b . a) . h) . g))
+	'((f . e) (((d . c) b . a) . h) . g))
 
 ;;; 6. Define a function CONS-CELL-COUNT that counts the number of CONS
 ;;; cells (i.e. the number of pairs) in a given structure
+
+
 
 (check= (cons-cell-count '()) 0)
 (check= (cons-cell-count '(a . b)) 1)
@@ -122,6 +131,8 @@ use REMOVE.
 ;;; solve this without changing the method's signature. Nor should you
 ;;; add an accumulator or auxilliary variable in a help method.
 
+
+
 (thm (implies (pos-bbp x)
               (= (* 2 (bb-to-n x)) (bb-to-n (cons nil x)))))
 
@@ -130,37 +141,59 @@ use REMOVE.
 ;;; of the first x in l.
 
 
+
 ;;; 9. Write a function ZIP-LISTS that takes two lists l1 and
 ;;; l2. ZIP-LISTS returns a list formed from pairs of elements taken
 ;;; from with the car coming from l1 and the cdr coming from l2. If
 ;;; the lists are of uneven length, then drop the tail of the longer
 ;;; one.
 
+
+
 (test? (implies (and (tlp l1) (tlp l2))
 		(= (len (zip-lists l1 l2)) (min (len l1) (len l2)))))
 
 
-;;; 10. Write a function WALK-SYMBOL that takes a symbol x and an
-;;; association list s. Your function should walk through s for the
-;;; value associated with x. If the associated value is a symbol, it
-;;; too must be walked in s. If x has no association, then WALK-SYMBOL
-;;; should return x. 
+#| 
+
+  We will do this one in program mode. Then, we will go back to logic
+  mode. The termination arguments for this are tricky! Really, it's
+  not the function that's tricky: as written, it's just /not/
+  guaranteed to terminate! Rather, what's tricky is describing the
+  subset of alists on which it *is* guaranteed to terminate.
+
+|#
+
+:program
+
+;;; 10. Write a function WALK-SYMBOL that takes either an atom (almost
+;;; always a symbol) and an association list s. Your function should
+;;; walk through s for the value associated with x. If the associated
+;;; value is a symbol, it too must be walked in s. If x has no
+;;; association, then WALK-SYMBOL should return x.
+
+
 
 (check= (walk-symbol 'a '((a . 5))) 5)
-(check= (walk-symbol 'a '((b . c) (a . b))) c)
+(check= (walk-symbol 'a '((b . c) (a . b))) 'c)
 (check= (walk-symbol 'a '((a . 5) (b . 6) (c . a))) 5)
 (check= (walk-symbol 'c '((a . 5) (b . (a . c)) (c . a))) 5)
-(check= (walk-symbol 'b '((a . 5) (b . ((c . a))) (c . a))) ((c . a)))
+(check= (walk-symbol 'b '((a . 5) (b . ((c . a))) (c . a))) '((c . a)))
 (check= (walk-symbol 'd '((a . 5) (b . (1 2)) (c . a) (e . c) (d . e))) 5)
-(check= (walk-symbol 'd '((a . 5) (b . 6) (c . f) (e . c) (d . e))) f)
+(check= (walk-symbol 'd '((a . 5) (b . 6) (c . f) (e . c) (d . e))) 'f)
 
-;;; 11. Write a function UNZIP-LISTS that, given two sorted lists of nats
-;;; without duplicates, returns a sorted list of nats without
-;;; duplicates containing the elements of both lists. 
+:logic
 
+;;; 11. Write a function UNZIP-LISTS that recursively deconstructs a
+;;; list of pairs and returns a list of the cars in order followed by
+;;; a list of the cdrs also in order.
+
+
+
+(check= (unzip-lists '((a . b) (c . d) (e . f))) '((a c e) (b d f)))
 (check= (unzip-lists '((()))) '((()) ()))
 
-(test? (implies (lopp e)
+(test? (implies (alistp e)
 		(equal (let ((v (unzip-lists e)))
 			 (zip-lists (car v) (cdr v)))
 		       e)))
