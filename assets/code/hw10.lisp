830d72f4c0591a2bb605052d101bd589954370f2
Jason Hemann <jasonhemann@gmail.com>
2020-11-09 22:49:57 -0500

    adding addtl to sched

--- assets/code/hw10.lisp
+++ assets/code/hw10.lisp
@@ -1,6 +1,6 @@
 #| 
 
-HW 9: The Proof of the Pudding
+HW 10: The Proof of the Pudding
 
 |#
 
@@ -38,67 +38,62 @@ different sort.
       ans
       (fib-acc2 (1- c) ans (+ ans-1 ans))))
 
-;; Your task is to prove:
-
+;; Our conjecture
 (<= (fib2 n) (fact n))
 
-;; Go wild! 
-
-(<= (fib2 n) (fact n))
-= {Lemma fib-fib2}
-(<= (fib n) (fact n))
-
-;; Induction on x via fib
-
-(implies (not (natp n)) (<= (fib n) (fact n)))
-
-(implies (and (natp n) (< n 2)) (<= (fib n) (fact n)))
-
-(implies (and (natp n)
-	      (not (< n 2))
-	      (<= (fib (- n 2)) (fact (- n 2)))
-	      (<= (fib (- n 1)) (fact (- n 1))))
-	 (<= (fib n) (fact n)))
+;; Go to it!
 
 
+;; Let me gently hint that, you will very likely want to show first
 
-(implies (and (natp n)
-	      (not (< n 2))
-	      (<= (fib (- n 2)) (fact (- n 2)))
-	      (<= (fib (- n 1)) (fact (- n 1))))
-	 (<= (fib n) (fact n)))
+(equal (fib2 n) (fib n))
 
-
-;; Context
-C1 (natp n)
-C2 (not (< n 2))
-C3 (<= (fib (- n 2)) (fact (- n 2)))
-C4 (<= (fib (- n 1)) (fact (- n 1)))
-
-;; DC 
+;; and use that in constructing the goal for your proof.
 
 
-;; Goal
-(<= (fib n) (fact n))
+#|
 
-;; Proof
-(<= (fib n) (fact n))
-= {def fib, fact}
-(<=  (if (< n 2)
-	 n
-       (+ (fib (1- n)) (fib (1- (1- n)))))
-     (if 0 1
-       (* n (fact (1- n)))))
-= { }
-(<=  (+ (fib (1- n)) (fib (1- (1- n)))) (* n (fact (1- n))))
-= { }
+Part II: A not-part of your homework.
 
-(<= (+ (fib (- n 2)) (fib (- n 1)))
-    (+ (fact (- n 2)) (fact (- n 1))))
-;; Lemma
-(<= (+ (fact (- n 2)) (fact (- n 1))) (* n (fact (1- n))))
+You all. This is not a part of your homework. You are not required to
+turn anything in for this. However, if you treat it like your
+homework, you'll be better prepared. I would like each posse to meet
+with me via Zoom, for approximately ~10m, after having sent a
+suggestion of the statement that they wish to prove in ACL2. Using
+ACL2, to do actual theorem-proving work, not just pen-and-paper for
+it.
 
-(<= (+ (fact (- n 2)) (fact (- n 1))) (* n (fact (1- n))))
+Alternately, if your group has some other kind of idea for something
+related to theorem proving---if for instance your mob has experience
+with Agda and you want to pursue something related to that, that's a
+suggestion. If you find yourselves really intrigued by Prolog or
+miniKanren and want to try your hand with something related that way,
+I can help guide you. If you are by chance interested in non-classical
+logics or alternate proof systems and think you've got something
+interesting to say or to do here (if any, I would suggest trying to
+make an educational, pedagogical contribution) that too might strike
+your fancy.
 
-;; Lemma
-(<= (+ (fact (1- n)) (fact (1- n))) (* n (fact (1- n))))
+But please be prepared to schedule a meeting with me on or hopefully
+between this Saturday the 14th and Wednesday the 18th of next week.
+When you schedule that meeting, send me a couple of paragraphs
+describing what you want to do. No need for grammatical flourishes; I
+want to put you all on a good, right track ASAP. Maybe have a back-up
+idea in your back pocket?
+
+FWIW, I'm expecting something maybe less ambitious in scope that we
+did mid-semester, but also consider mechanical proofs will likely not
+require nearly so much documentation. Think about something slightly
+more than these 1-week homework questions, but certainly less than the
+Church-Rosser theorem.
+
+I'll ask to receive a progress report from you all on the 30th, when
+we return from break, with an ultimate due date Wed, Dec
+9th. Presentations will have to be due on or before the 9th also.
+
+At student's/mob's requests, I can be persuaded to, for just this once
+to accept some late assignments---say, give you the weekend to put on
+finishing touches, and to have met with me on or before the evening of
+the 14th. However, I certainly can't and wouldn't mandate that.
+
+|# 
